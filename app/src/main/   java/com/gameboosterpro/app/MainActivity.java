02fcb047228b40d2c9e2ccd1bc4821a7321690e0
ashiquea5541-creator <ashiquea5541@gmail.com>
2026-06-21 13:01:19 +0500

    Update MainActivity.java

--- app/src/main/   java/com/gameboosterpro/app/MainActivity.java
+++ app/src/main/   java/com/gameboosterpro/app/MainActivity.java
@@ -6,20 +6,18 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.LinearLayout;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle b) {
         super.onCreate(b);
         Button btn = new Button(this);
-        btn.setText("Start Bubble");
+        btn.setText("Start Booster");
         btn.setOnClickListener(v -> {
             if (!Settings.canDrawOverlays(this)) {
-                Intent i = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                        Uri.parse("package:" + getPackageName()));
-                startActivity(i);
-                Toast.makeText(this, "Permission do, phir dubara button dabao", 1).show();
+                startActivity(new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:" + getPackageName())));
             } else {
                 startService(new Intent(this, BubbleService.class));
                 finish();
